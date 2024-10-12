85207aae3ae22147c6492458e4ca197dbe598dea
PremiumCharity <premiumcharitycompany@gmail.com>
2024-10-11 23:38:03 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function ShowSidebar(){
+    const sidebar = document.querySelector(`.sidebar`)
+    sidebar.style.width='60%'
+}
+
+function HideSidebar(){
+    const sidebar = document.querySelector(`.sidebar`)
+    sidebar.style.width='0px'
+}
